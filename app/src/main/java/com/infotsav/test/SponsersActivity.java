d46c6f6a4004f35ae7647222d93fc2823fdb29ce
Siddharth Vaish <sidvaish97@gmail.com>
2018-12-07 04:06:36 +0530

    new activities add for each card title and app_name changed

---
+++ app/src/main/java/com/infotsav/test/SponsersActivity.java
@@ -0,0 +1,13 @@
+package com.infotsav.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SponsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sponsers);
+    }
+}
---
+++ app/src/main/java/com/infotsav/test/UpdatesActivity.java
@@ -0,0 +1,13 @@
+package com.infotsav.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updates);
+    }
+}
