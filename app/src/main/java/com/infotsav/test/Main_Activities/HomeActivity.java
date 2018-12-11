057fbc38e5fa5808206e20763fc281af9b00b02f
veloxIsTesting <aviharsh234>
2018-12-11 22:50:59 +0530

    firebase_init

--- app/src/main/java/com/infotsav/test/Main_Activities/HomeActivity.java
+++ app/src/main/java/com/infotsav/test/Main_Activities/HomeActivity.java
@@ -3,6 +3,8 @@ package com.infotsav.test.Main_Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.infotsav.test.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -11,5 +13,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //TODO: Read the firebase docs for realtime database or firestore (which ever you prefer)
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 }
