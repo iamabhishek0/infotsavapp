e66c16afeddc4901bfceb57469200e047841e14f
Himadri Singh <himsingh1612@gmail.com>
2018-12-11 16:31:15 +0530

    sponsors adapter added,layout created himadri

---
+++ app/src/main/java/com/infotsav/test/Main_Activities/SponsorsAdapter.java
@@ -0,0 +1,58 @@
+package com.infotsav.test.Main_Activities;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.infotsav.test.R;
+
+import java.util.List;
+
+public class SponsorsAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<Sponsors_details> mSponsors_detailslist;
+
+
+    public SponsorsAdapter(Context mContext, List<Sponsors_details> mContactus_detailslist) {
+        this.mContext = mContext;
+        this.mSponsors_detailslist = mContactus_detailslist;
+    }
+
+    @Override
+    public int getCount() {
+        return mSponsors_detailslist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mSponsors_detailslist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = View.inflate(mContext,R.layout.sponsors_list,null);
+        TextView person_name=(TextView)view.findViewById(R.id.person_name);
+        TextView person_email=(TextView)view.findViewById(R.id.person_email);
+        TextView person_department=(TextView)view.findViewById(R.id.person_department);
+        TextView person_call=(TextView)view.findViewById(R.id.call_person_button);
+        ImageView person_image=(ImageView)view.findViewById(R.id.person_photo);
+
+
+
+        person_name.setText(mSponsors_detailslist.get(position).getName());
+        person_email.setText(mSponsors_detailslist.get(position).getSponsorship1());
+        person_department.setText(mSponsors_detailslist.get(position).getSponsorship2());
+        person_image.setImageResource(mSponsors_detailslist.get(position).getImage());
+        view.setTag(mSponsors_detailslist.get(position).getId());
+        return view;
+    }
+}
---
+++ app/src/main/java/com/infotsav/test/Main_Activities/Sponsors_details.java
@@ -0,0 +1,57 @@
+package com.infotsav.test.Main_Activities;
+
+public class Sponsors_details {
+    private int id;
+    private String name;
+    private String sponsorship1;
+    private String sponsorship2;
+    private int image;
+
+    public Sponsors_details(int id, String name, String sponsorship1, String sponsorship2, int image) {
+        this.id = id;
+        this.name = name;
+        this.sponsorship1 = sponsorship1;
+        this.sponsorship2 = sponsorship2;
+        this.image=image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSponsorship1() {
+        return sponsorship1;
+    }
+
+    public String getSponsorship2() {
+        return sponsorship2;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSponsorship1(String sponsorship2) {
+        this.sponsorship1 = sponsorship1;
+    }
+
+    public void setSponsorship2(String sponsorship2) {
+        this.sponsorship2 = sponsorship2;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
