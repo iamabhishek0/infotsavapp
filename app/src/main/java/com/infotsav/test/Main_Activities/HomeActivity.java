534356153e52b9037829934d3ba7cf83debe7523
Siddharth Vaish <sidvaish97@gmail.com>
2018-12-13 13:44:11 +0530

    changes in home activity

--- app/src/main/java/com/infotsav/test/Main_Activities/HomeActivity.java
+++ app/src/main/java/com/infotsav/test/Main_Activities/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.infotsav.test.Main_Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.infotsav.test.R;
+import com.infotsav.test.foldingView.FoldingActivity;
 
 public class HomeActivity extends AppCompatActivity {
     
@@ -15,5 +18,11 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         //TODO: Read the firebase docs for realtime database or firestore (which ever you prefer)
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void launchFoldingActivity(View view) {
+
+        Intent intent = new Intent(this, FoldingActivity.class);
+        startActivity(intent);
     }
 }
