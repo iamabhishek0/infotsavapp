16ce8424bde41865b44dc9321733f16d25b96f0b
Siddharth Vaish <sidvaish97@gmail.com>
2018-12-14 18:03:20 +0530

    fonts added

--- app/src/main/java/com/infotsav/test/foldingView/FoldingActivity.java
+++ app/src/main/java/com/infotsav/test/foldingView/FoldingActivity.java
@@ -1,5 +1,6 @@
 package com.infotsav.test.foldingView;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
